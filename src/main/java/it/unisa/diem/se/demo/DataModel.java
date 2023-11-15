32d50613297a5665a6ce2d2960cc69214828bc13
Eugenio2001 <e.iannaccone9@studenti.unisa.it>
2023-11-15 17:11:27 +0100

    Terzo commit, scrittura bottoni in textfield

--- src/main/java/it/unisa/diem/se/demo/DataModel.java
+++ src/main/java/it/unisa/diem/se/demo/DataModel.java
@@ -1,4 +1,21 @@
 package it.unisa.diem.se.demo;
 
 public class DataModel {
+
+    private String firstNumber = "";
+
+    public void addNumber(int number)
+    {
+         firstNumber += number;
+    }
+
+    public void resetNumber()
+    {
+        firstNumber = "";
+    }
+
+    public String getFirstNumber() {
+        return firstNumber;
+    }
+    
 }
--- src/main/java/it/unisa/diem/se/demo/HelloController.java
+++ src/main/java/it/unisa/diem/se/demo/HelloController.java
@@ -6,57 +6,77 @@ import javafx.scene.control.TextField;
 
 public class HelloController {
 
+    private DataModel model = new DataModel();
+
     @FXML
     private TextField textField;
 
+    private void updateTexfield() {
+        textField.setText(model.getFirstNumber());
+    }
+
+    private void resetTextfield() {
+        textField.setText("");
+    }
+
     @FXML
     void button0Clicked(ActionEvent event) {
-
+        model.addNumber(0);
+        updateTexfield();
     }
 
     @FXML
     void button1Clicked(ActionEvent event) {
-
+        model.addNumber(1);
+        updateTexfield();
     }
 
     @FXML
     void button2Clicked(ActionEvent event) {
-
+        model.addNumber(2);
+        updateTexfield();
     }
 
     @FXML
     void button3Clicked(ActionEvent event) {
-
+        model.addNumber(3);
+        updateTexfield();
     }
 
     @FXML
     void button4Clicked(ActionEvent event) {
-
+        model.addNumber(4);
+        updateTexfield();
     }
 
     @FXML
     void button5Clicked(ActionEvent event) {
-
+        model.addNumber(5);
+        updateTexfield();
     }
 
     @FXML
     void button6Clicked(ActionEvent event) {
-
+        model.addNumber(6);
+        updateTexfield();
     }
 
     @FXML
     void button7Clicked(ActionEvent event) {
-
+        model.addNumber(7);
+        updateTexfield();
     }
 
     @FXML
     void button8Clicked(ActionEvent event) {
-
+        model.addNumber(8);
+        updateTexfield();
     }
 
     @FXML
     void button9Clicked(ActionEvent event) {
-
+        model.addNumber(9);
+        updateTexfield();
     }
 
     @FXML
@@ -66,7 +86,8 @@ public class HelloController {
 
     @FXML
     void cancelAction(ActionEvent event) {
-
+        resetTextfield();
+        model.resetNumber();
     }
 
     @FXML
