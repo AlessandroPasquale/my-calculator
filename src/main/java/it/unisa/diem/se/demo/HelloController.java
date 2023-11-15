b4287ad71df89fe0101aa826ef569face11a766c
Marco Martelli <m.martelli4@studenti.unisa.it>
2023-11-15 16:22:26 +0100

    Aggiornamento view xml e creazione del controller

--- src/main/java/it/unisa/diem/se/demo/HelloController.java
+++ src/main/java/it/unisa/diem/se/demo/HelloController.java
@@ -1,14 +1,92 @@
 package it.unisa.diem.se.demo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField textField;
+
+    @FXML
+    void button0Clicked(ActionEvent event) {
+
     }
+
+    @FXML
+    void button1Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button2Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button3Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button4Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button5Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button6Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button7Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button8Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button9Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void calculateAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancelAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void divideAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void multAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void subAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sumAction(ActionEvent event) {
+
+    }
+
 }
